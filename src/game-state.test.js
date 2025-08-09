a4f205c5a2398356604ace6efb571c9baa42a1cf
macaronpark <pxxxsxzy.0@gmail.com>
2025-08-09 16:59:44 +0900

    test: GameState 공개 API를 사용해 테스트하도록 수정

--- src/game-state.test.js
+++ src/game-state.test.js
@@ -1,51 +1,121 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
-import { GameState } from './game-state';
 import { Mushroom } from './mushroom';
 import { EventBus } from './event-bus';
 import { CONFIG } from './config';
+import { createGameState } from './game-state';
 
-describe('deleteMushroom', () => {
-  const fieldID = 'field-1';
-  const mushroom = new Mushroom({
+let GameState;
+let fieldID;
+let mushroom;
+
+beforeEach(() => {
+  // Given
+  fieldID = 'field-1';
+  mushroom = new Mushroom({
     fieldID,
     mushroomType: CONFIG.MUSHROOM.RED_CAP.type,
   });
 
+  GameState = createGameState();
+  GameState.init();
+});
+
+describe('addNewMushroom', () => {
+  beforeEach(() => {
+    // When
+    EventBus.emit({
+      from: 'TEST',
+      e: CONFIG.EVENT_ID.GAME_STATE.SET_NEW_MUSHROOM,
+      data: { ...mushroom },
+    });
+  });
+
+  it('밭 정보에 신규 버섯 정보를 추가한다', () => {
+    // Then
+    const { fields } = GameState.getState();
+    expect(fields[fieldID].mushroomID).toBe(mushroom.id);
+  });
+
+  it('버섯 정보에 신규 버섯 정보를 추가한다', () => {
+    // Then
+    const { mushrooms } = GameState.getState();
+    expect(mushrooms[mushroom.id]).toEqual(mushroom);
+  });
+});
+
+describe('updateMushroomGrowthStage', () => {
+  it('버섯 성장 단계를 업데이트한다', () => {
+    // Given
+    const GROWTH_STAGE = CONFIG.GROWTH_STAGE.FRUITING;
+
+    // When
+    EventBus.emit({
+      from: 'TEST',
+      e: CONFIG.EVENT_ID.GAME_STATE.UPDATE_MUSHROOM_GROWTH_STAGE,
+      data: {
+        mushroomID: mushroom.id,
+        nextGrowthStage: GROWTH_STAGE,
+      },
+    });
+
+    // Then
+    const { mushrooms } = GameState.getState();
+    expect(mushrooms[mushroom.id].growthStage).toBe(GROWTH_STAGE);
+  });
+});
+
+describe('deleteMushroom', () => {
   let spyOnEmit;
 
   beforeEach(() => {
+    // Given
     spyOnEmit = vi.spyOn(EventBus, 'emit');
-    // TODO: 외부에서 수정할 수 없게하되 테스트는 가능하게 변경 필요
-    // 테스트용 헬퍼 함수? mushroom 속성을 밖에서 변경하지 못하게 하기?
-    GameState.mushrooms = {
-      [mushroom.id]: {
-        ...mushroom,
-        growthStage: CONFIG.GROWTH_STAGE.MATURE,
+
+    EventBus.emit({
+      from: 'TEST',
+      e: CONFIG.EVENT_ID.GAME_STATE.SET_NEW_MUSHROOM,
+      data: { ...mushroom },
+    });
+
+    const GROWTH_STAGE = CONFIG.GROWTH_STAGE.MATURE;
+
+    EventBus.emit({
+      from: 'TEST',
+      e: CONFIG.EVENT_ID.GAME_STATE.UPDATE_MUSHROOM_GROWTH_STAGE,
+      data: {
+        mushroomID: mushroom.id,
+        nextGrowthStage: GROWTH_STAGE,
       },
-    };
+    });
+
+    // When
+    EventBus.emit({
+      from: 'TEST',
+      e: CONFIG.EVENT_ID.GAME_STATE.HARVEST_MUSHROOM,
+      data: { fieldID, mushroomID: mushroom.id },
+    });
   });
 
   afterEach(() => {
-    vi.resetAllMocks();
+    spyOnEmit.mockRestore();
   });
 
   it('밭의 버섯 정보를 제거한다', () => {
-    GameState.deleteMushroom({ fieldID, mushroomID: mushroom.id });
-
-    expect(GameState.fields[fieldID].mushroomID).toBeNull();
+    // Then
+    const { fields } = GameState.getState();
+    expect(fields[fieldID].mushroomID).toBeNull();
   });
 
   it('버섯 정보를 제거한다', () => {
-    GameState.deleteMushroom({ fieldID, mushroomID: mushroom.id });
-
-    expect(GameState.mushrooms[mushroom.id]).toBeUndefined();
+    // Then
+    const { mushrooms } = GameState.getState();
+    expect(mushrooms[mushroom.id]).toBeUndefined();
   });
 
   it.todo('도감 해금 여부를 확인하고 필요 시 업데이트한다');
 
   it('버섯 정보 제거 후 UI 이벤트를 발생시킨다', () => {
-    GameState.deleteMushroom({ fieldID, mushroomID: mushroom.id });
-
+    // Then
     expect(spyOnEmit).toHaveBeenCalledWith(
       expect.objectContaining({
         from: CONFIG.MODULE_ID.GAME_STATE,
