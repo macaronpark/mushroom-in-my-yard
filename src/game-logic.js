d45657746f54614d15a2a230f3e7f3ad924b5221
macaronpark <pxxxsxzy.0@gmail.com>
2025-08-12 18:00:15 +0900

    feat: 버섯 종류를 랜덤하게 심도록 구현

--- src/game-logic.js
+++ src/game-logic.js
@@ -111,7 +111,7 @@ export function growTo({ mushroomID, nextGrowthStage }) {
 export function plantNewMushroom({ fieldID }) {
   const newMushroom = new Mushroom({
     fieldID,
-    mushroomType: CONFIG.MUSHROOM.RED_CAP.type,
+    mushroomType: getRandomMushroomType(),
   });
 
   EventBus.emit({
@@ -119,6 +119,29 @@ export function plantNewMushroom({ fieldID }) {
     e: CONFIG.EVENT_ID.GAME_STATE.SET_NEW_MUSHROOM,
     data: { ...newMushroom },
   });
+}
+
+export function getRandomMushroomType({
+  mushroomConfig = CONFIG.MUSHROOM,
+  rng = Math.random,
+} = {}) {
+  const types = Object.keys(mushroomConfig);
+  const weights = types.map((type) => mushroomConfig[type].rarity);
+
+  const cumulativeWeights = weights.reduce((acc, cur, i) => {
+    const value = i === 0 ? cur : acc[i - 1] + cur;
+    acc.push(value);
+
+    return acc;
+  }, []);
+
+  const randomNum = rng() * cumulativeWeights[cumulativeWeights.length - 1];
+
+  for (const i in cumulativeWeights) {
+    if (randomNum < cumulativeWeights[i]) {
+      return types[i];
+    }
+  }
 }
 
 export function harvestMushroom({ fieldID, mushroomID }) {
