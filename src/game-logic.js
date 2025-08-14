30ec5a3fe011a88a4b7deec20ea726d9c9cf725f
macaronpark <pxxxsxzy.0@gmail.com>
2025-08-14 16:06:06 +0900

    refactor: 효율적인 버섯 성장 체크를 위해 폴링에서 스케줄링 방식으로 변경

--- src/game-logic.js
+++ src/game-logic.js
@@ -7,10 +7,6 @@ const GameLogic = createGameLogic();
 export default GameLogic;
 
 function createGameLogic() {
-  const setGameLoop = () => {
-    setInterval(() => checkMushroomGrowth(), 300);
-  };
-
   const bindEvents = () => {
     EventBus.on({
       from: FROM,
@@ -21,104 +17,27 @@ function createGameLogic() {
 
   return {
     init() {
-      setGameLoop();
       bindEvents();
     },
   };
 }
 
 export function handleFieldClick({ fieldID }) {
-  const { mushroomID } = GameState.getField({ fieldID });
+  const field = GameState.getField({ fieldID });
 
-  if (!mushroomID) {
-    plantNewMushroom({ fieldID });
+  if (!field.mushroomID) {
+    const mushroomType = getRandomMushroomType();
+    const newMushroom = new Mushroom({ fieldID, mushroomType });
+
+    plant({ mushroom: newMushroom });
     return;
   }
 
-  const mushroom = GameState.getMushroom({ mushroomID });
-  if (mushroom.growthStage !== CONFIG.GROWTH_STAGE.MATURE) {
-    return;
-  }
+  const mushroom = GameState.getMushroom({ mushroomID: field.mushroomID });
+  const isGrowing = mushroom.growthStage !== CONFIG.GROWTH_STAGE.MATURE;
+  if (isGrowing) return;
 
-  harvestMushroom({ fieldID, mushroomID: mushroom.id });
-}
-
-export function checkMushroomGrowth() {
-  const mushroomList = GameState.getMushroomList();
-
-  mushroomList.forEach((mushroom) => {
-    const { fieldID, plantedAt, growthTime, growthStage } = mushroom;
-    const now = Date.now();
-
-    if (!shouldMushroomGrow({ plantedAt, growthTime, growthStage, now }))
-      return;
-
-    const nextGrowthStage = getNextGrowthStage({
-      current: growthStage,
-    });
-
-    if (!nextGrowthStage) return;
-
-    growTo({
-      fieldID,
-      mushroomID: mushroom.id,
-      nextGrowthStage,
-    });
-  });
-}
-
-export function shouldMushroomGrow({
-  plantedAt,
-  growthTime,
-  growthStage,
-  now,
-}) {
-  if (growthStage === CONFIG.GROWTH_STAGE.MATURE) return false;
-
-  const requiredTimeForCurrentStage =
-    growthStage === CONFIG.GROWTH_STAGE.MYCELIUM
-      ? growthTime.myceliumToFruiting
-      : growthTime.fruitingToMature;
-
-  const elapsed = now - plantedAt;
-
-  return elapsed >= requiredTimeForCurrentStage;
-}
-
-export function getNextGrowthStage({ current }) {
-  const { MYCELIUM, FRUITING, MATURE } = CONFIG.GROWTH_STAGE;
-
-  switch (current) {
-    case MYCELIUM:
-      return FRUITING;
-
-    case FRUITING:
-      return MATURE;
-
-    default:
-      return null;
-  }
-}
-
-export function growTo({ mushroomID, nextGrowthStage }) {
-  EventBus.emit({
-    from: CONFIG.MODULE_ID.GAME_LOGIC,
-    e: CONFIG.EVENT_ID.GAME_STATE.UPDATE_MUSHROOM_GROWTH_STAGE,
-    data: { mushroomID, nextGrowthStage },
-  });
-}
-
-export function plantNewMushroom({ fieldID }) {
-  const newMushroom = new Mushroom({
-    fieldID,
-    mushroomType: getRandomMushroomType(),
-  });
-
-  EventBus.emit({
-    from: FROM,
-    e: CONFIG.EVENT_ID.GAME_STATE.SET_NEW_MUSHROOM,
-    data: { ...newMushroom },
-  });
+  harvest({ fieldID, mushroomID: mushroom.id });
 }
 
 export function getRandomMushroomType({
@@ -145,7 +64,75 @@ export function getRandomMushroomType({
   }
 }
 
-export function harvestMushroom({ fieldID, mushroomID }) {
+export function getNextGrowthStage({ current }) {
+  const { MYCELIUM, FRUITING, MATURE } = CONFIG.GROWTH_STAGE;
+
+  switch (current) {
+    case MYCELIUM:
+      return FRUITING;
+
+    case FRUITING:
+      return MATURE;
+
+    default:
+      return null;
+  }
+}
+
+function plant({ mushroom }) {
+  EventBus.emit({
+    from: FROM,
+    e: CONFIG.EVENT_ID.GAME_STATE.SET_NEW_MUSHROOM,
+    data: { ...mushroom },
+  });
+
+  const { schedule } = createScheduler({ growTo });
+  schedule({
+    mushroomID: mushroom.id,
+    current: mushroom.growthStage,
+    growthTime: mushroom.growthTime,
+  });
+}
+
+function createScheduler({ growTo }) {
+  let timerID = null;
+
+  const schedule = ({ mushroomID, current, growthTime }) => {
+    clearTimer();
+
+    const nextGrowthStage = getNextGrowthStage({ current });
+    if (!nextGrowthStage) return;
+
+    const delay =
+      current === CONFIG.GROWTH_STAGE.MYCELIUM
+        ? growthTime.myceliumToFruiting
+        : growthTime.fruitingToMature;
+
+    timerID = setTimeout(() => {
+      growTo({ mushroomID, nextGrowthStage });
+      schedule({ mushroomID, current: nextGrowthStage, growthTime });
+    }, delay);
+  };
+
+  function clearTimer() {
+    if (!timerID) return;
+
+    clearTimeout(timerID);
+    timerID = null;
+  }
+
+  return { schedule };
+}
+
+function growTo({ mushroomID, nextGrowthStage }) {
+  EventBus.emit({
+    from: CONFIG.MODULE_ID.GAME_LOGIC,
+    e: CONFIG.EVENT_ID.GAME_STATE.UPDATE_MUSHROOM_GROWTH_STAGE,
+    data: { mushroomID, nextGrowthStage },
+  });
+}
+
+function harvest({ fieldID, mushroomID }) {
   EventBus.emit({
     from: FROM,
     e: CONFIG.EVENT_ID.GAME_STATE.HARVEST_MUSHROOM,
